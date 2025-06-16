d83748012d6d223c9ddfe9a2d4c412c567b56043
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-06-16 17:10:31 +0200

    add custom path to the gateway to pass traffic to specified microservice

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,33 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+
+	//bean che serve per configurare una route alternativa da fornire nell url del gateway
+// rispetto a quella generata di default dal gateway
+	// in questo caso sto fornendo un url alternativo :
+	// http://localhost:8072/pincopallo/accounts/*
+	// invece del default http://localhost:8072/accounts/*
+	@Bean
+	public RouteLocator customRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes().route(p -> p.path("/pincopallo/accounts/**")
+				.filters(filter ->
+						filter.rewritePath(
+								"/pincopallo/accounts/(?<segment>.*)" ,
+								"/${segment}")
+				)
+				.uri("lb://ACCOUNTS"))
+				.build();
 	}
 
 }
